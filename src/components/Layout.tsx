c05c912c6239c82e30ca5fce1be4afa3edf35941
AntoineRai <antoine.raimand@gmail.com>
2025-05-27 20:36:57 +0200

    fix: header menu full responsive

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -34,7 +34,7 @@ const Header = ({ isMenuOpen, setIsMenuOpen, logo, linkClass, iconClass, isConta
         {/* Menu hamburger */}
         <button 
           onClick={() => setIsMenuOpen(!isMenuOpen)} 
-          className={`lg:hidden ${isContact ? 'text-black' : 'text-white'}`}
+          className={`2xl:hidden ${isContact ? 'text-black' : 'text-white'}`}
         >
           <svg 
             xmlns="http://www.w3.org/2000/svg" 
@@ -63,7 +63,7 @@ const Header = ({ isMenuOpen, setIsMenuOpen, logo, linkClass, iconClass, isConta
 
         {/* Menu mobile */}
         {isMenuOpen && (
-          <div className="lg:hidden absolute top-full left-0 right-0 bg-black bg-opacity-95 p-4 z-50">
+          <div className="2xl:hidden absolute top-full left-0 right-0 bg-black bg-opacity-95 p-4 z-50">
             <nav className="flex flex-col space-y-4">
               <Link 
                 to="/" 
@@ -105,6 +105,16 @@ const Header = ({ isMenuOpen, setIsMenuOpen, logo, linkClass, iconClass, isConta
                   </svg>
                 </a>
                 <a 
+                  href="https://www.tiktok.com/@narcisseprojet" 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                  className="text-white hover:text-gray-300 transition-colors"
+                >
+                  <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
+                    <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-1-.05A6.33 6.33 0 0 0 5 20.1a6.34 6.34 0 0 0 10.86-4.43v-7a8.16 8.16 0 0 0 4.77 1.52v-3.4a4.85 4.85 0 0 1-1-.1z"/>
+                  </svg>
+                </a>
+                <a 
                   href="https://www.linkedin.com/company/narcisse-prod/" 
                   target="_blank" 
                   rel="noopener noreferrer"
@@ -112,6 +122,16 @@ const Header = ({ isMenuOpen, setIsMenuOpen, logo, linkClass, iconClass, isConta
                 >
                   <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
                     <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
+                  </svg>
+                </a>
+                <a 
+                  href="https://www.facebook.com/profile.php?id=61567367669025" 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                  className="text-white hover:text-gray-300 transition-colors"
+                >
+                  <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
+                    <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
                   </svg>
                 </a>
               </div>
